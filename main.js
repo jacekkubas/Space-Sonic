b71f7a95a972e12b4588221a07b1b787dd6f941b
Jacek <jacenty.kubas@gmail.com>
2018-07-02 14:42:42 +0200

    clearing

--- main.js
+++ main.js
@@ -1,41 +1,8 @@
-$(document).ready(function() {
+window.onload = function () {
+    var canvas = document.querySelector('canvas');
+    var btns = document.querySelector('.btns');
     
-    $.fn.serializeFormJSON = function () {
-
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function () {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
-        });
-        return o;
-    };
-    
-    $('#wyniki').submit(function(e) {
-        e.preventDefault();
-        var data = $(this).serializeFormJSON();
-    });
-    
-    $('#form').submit(function(e) {
-        e.preventDefault();
-        var value = $('#form input').val();
-        $('#wyniki input:first-child').val(value);
-        $('canvas').addClass('active');
-        $('.btns').addClass('active');
-        $('#form button, #form input').blur();
-        startGame();
-    });
-    
-    // auto start
-    window.onload = function () {
-        $('canvas').addClass('active');
-        $('.btns').addClass('active');
-        startGame();
-    }
-})
+    canvas.classList.add('active');
+    btns.classList.add('active');
+    startGame();
+}
